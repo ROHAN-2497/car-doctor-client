7092c5c05a8a1cb2b6590997f2a4e2642f7a6aab
rohan future devoloper <xrohaislam373@gmail.com>
2023-07-23 21:57:05 +0600

    localStorage setItem

--- src/Routes/PrivateRoute/PrivateRoute.jsx
+++ src/Routes/PrivateRoute/PrivateRoute.jsx
@@ -1,16 +1,18 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../components/Providers/authProvider';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 const PrivateRoute = ({children}) => {
     const {users, loading} = useContext(AuthContext);
+    const location = useLocation();
+
     if(users?.email){
         return children;
     }
     if(loading){
         return <progress className="progress w-56"></progress>
     }
-    return <Navigate to="/login" replace></Navigate>
+    return <Navigate to="/login" state={{from : location}} replace></Navigate>
 };
 
 export default PrivateRoute;
--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -7,7 +7,7 @@ const Bookings = () => {
 
   const [bookings, setBookings] = useState([]);
 
-  const url = `http://localhost:5000/booking?email=${users?.email}`;
+  const url = `http://localhost:5000/bookings?email=${users?.email}`;
   useEffect(() => {
     fetch(url)
       .then((res) => res.json())
@@ -32,21 +32,21 @@ const Bookings = () => {
 
     const handleBookingConfirm = (id) => {
       fetch(`http://localhost:5000/bookings/${id}`, {
-        method: 'PATCH',
+        method: "PATCH",
         headers: {
-          'content-type' : 'application/json'
+          "content-type": "application/json",
         },
-        body: JSON.stringify({status: 'confirm'})
+        body: JSON.stringify({ status: 'confirm' }),
       })
         .then((res) => res.json())
         .then((data) => {
           console.log(data);
           if (data.modifiedCount > 0) {
             // Update Status
-            const remaining = bookings.filter(booking => booking._id !== id);
-            const updated = bookings.find (booking => booking._id === id );
-            updated.status = 'confirm'
-            const newBookings = [updated, ...remaining]
+            const remaining = bookings.filter((booking) => booking._id !== id);
+            const updated = bookings.find((booking) => booking._id === id);
+            updated.status = "confirm";
+            const newBookings = [updated, ...remaining];
             setBookings(newBookings);
           }
         });
